4c988c002593694e2043c66b87afbb1e406b0512
David Suarez <dsuarey@gmail.com>
2018-09-16 19:43:54 -0700

    Done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-(function() {
-    // Write code here
-})()
+// Increase slider by 1
+console.log('JS LOADED')
+
+
+
+function volumeAdjuster() {
+    let input = document.getElementById('start') 
+    input.addEventListener('change', function () {
+        document.getElementById('volume-level').innerHTML = this.value
+    })
+}
+volumeAdjuster();
+
